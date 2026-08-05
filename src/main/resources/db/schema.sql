8c74d110b1e285303b6e8ed47d967002b3b64a59
NE20253030 <20253030-eginanasa@hcs.ac.jp>
2026-08-05 19:40:50 +0900

    DBスキーマとダミーデータの作成

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,13 +1,46 @@
-/* テーブル削除：CREATE文と逆順で記載する */
+/*
+--テーブルがすでに存在しています　というようなエラーが出たとき、こいつを実行してからやり直してください
+DROP TABLE IF EXISTS input_logs;
+DROP TABLE IF EXISTS students;
+DROP TABLE IF EXISTS error_reasons;
+DROP TABLE IF EXISTS courses;
+*/
 
-DROP TABLE IF EXISTS user_m;
+/*
+SERIAL　自動連番を振ります
+TIMESTAMP DEFAULT CURRENT_TIMESTAMP　送信された時刻を記録します
+ON DELETE SET NULL　外部キーを含むデータが消すとき、そこのデータをNULLにして消していいよと許可します
+*/
 
-/* ユーザーマスタ */
-CREATE TABLE
-  user_m (
-    user_id VARCHAR(50) PRIMARY KEY,
-    PASSWORD VARCHAR(100),
-    user_name VARCHAR(50),
-    ROLE VARCHAR(50),
-    enabled BOOLEAN
-  );
+-- 1. コース情報 テーブル
+CREATE TABLE courses (
+    course_id SERIAL PRIMARY KEY,              -- コースID　自動連番が望ましいです
+    course_name VARCHAR(100) NOT NULL,		-- コース名
+    start_date DATE NOT NULL,		-- 開始日
+    end_date DATE NOT NULL		-- 終了日
+);
+
+-- 2. ミス原因マスタ
+CREATE TABLE error_reasons (
+    reason_id SERIAL PRIMARY KEY,	-- ミス原因ID
+    reason_name VARCHAR(100) NOT NULL	-- ミス原因の内容
+);
+
+-- 3. 生徒情報 テーブル
+CREATE TABLE students (
+    student_id VARCHAR(20) PRIMARY KEY,        -- 学籍番号など
+    course_id INT REFERENCES courses(course_id),-- 外部キー　コースID
+    student_name VARCHAR(50) NOT NULL		-- 生徒名
+);
+
+-- 4. 入力ログDB
+CREATE TABLE input_logs (
+    log_id SERIAL PRIMARY KEY,		-- ログID　こちらも自動連番が望ましいです
+    student_id VARCHAR(20) REFERENCES students(student_id) ON DELETE SET NULL,-- 生徒ID
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- 送信日時
+    course_id INT REFERENCES courses(course_id) ON DELETE SET NULL,-- コースID
+    reason_id INT REFERENCES error_reasons(reason_id) ON DELETE SET NULL,-- ミス原因ID
+    correct_answer TEXT,-- 正答
+    incorrect_answer TEXT,-- 誤答
+    question_text TEXT -- 問題文　後でNoSQLに逃がす？
+);
